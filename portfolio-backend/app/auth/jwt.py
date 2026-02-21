cec75ace3b455b43ab80f499bd3c79e4c4d0a8a3
Andres Franco <andresmfrancoc@gmail.com>
2026-02-20 18:34:12 -0600

    fix github security errors

--- portfolio-backend/app/auth/jwt.py
+++ portfolio-backend/app/auth/jwt.py
@@ -1,5 +1,5 @@
 from datetime import datetime, timedelta
-from jose import jwt
+import jwt
 from app.core.config import settings
 
 def create_access_token(data: dict, expires_delta: timedelta = None):
